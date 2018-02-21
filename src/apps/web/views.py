83a76a6d52c140d8de19acd50784818646970dba
rovinei <ro.vinei@yahoo.com>
2018-02-22 02:27:16 +0700

    add home page

--- src/apps/web/views.py
+++ src/apps/web/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 
+def home_page(request):
+    context = dict()
+    context.update({
+        'meta': {
+            'page_title': 'Messenger Bot for CRM | Welcome'
+        }
+    })
+    return render(request=request, template_name='web/home.html', context=context)
+
+
 def privacy_page(request):
     context = dict()
     context.update({
