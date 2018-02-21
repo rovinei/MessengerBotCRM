af80e0ab9454b63521a6caef8aa655c975194fdd
rovinei <ro.vinei@yahoo.com>
2018-02-22 02:28:11 +0700

    add home page

--- src/apps/web/urls.py
+++ src/apps/web/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "web"
 
 urlpatterns = [
-    path('/', views.home_page, name='home_page'),
+    path('', views.home_page, name='home_page'),
     path('privacy/', views.privacy_page, name='privacy_page'),
     path('term-of-service/', views.tos_page, name='tos_page'),
 ]
