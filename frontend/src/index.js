d512f38636f263a29e12a7c803a27ea3367d9f9b
rovinei <ro.vinei@yahoo.com>
2018-03-13 19:29:01 +0700

    configure reactjs dashboard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter } from 'react-router-redux'
+import { Provider } from 'react-redux'
+import configureStore from './store'
 import {
     HashRouter,
     Route,
     Switch
 } from 'react-router-dom';
-
 import App from 'containers/App/App.jsx';
-
 import './assets/css/bootstrap.min.css';
 import './assets/css/animate.min.css';
 import './assets/sass/light-bootstrap-dashboard.css';
 import './assets/css/demo.css';
 import './assets/css/pe-icon-7-stroke.css';
 
-
+const history = createHistory()
+const store = configureStore(history)
 
 
 ReactDOM.render((
-    <HashRouter>
-        <Switch>
-            <Route path="/" name="Home" component={App}/>
-        </Switch>
-    </HashRouter>
+    <Provider store={store}>
+        <HashRouter history={history}>
+            <Switch>
+                <Route path="/" name="Home" component={App}/>
+            </Switch>
+        </HashRouter>
+    </Provider>
 ),document.getElementById('root'));
