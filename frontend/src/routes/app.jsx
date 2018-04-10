f7752a569d99e0ac11e6b150cc2135d3aacd1849
rovinei <ro.vinei@yahoo.com>
2018-04-11 06:52:22 +0700

    adjust importing dir

--- frontend/src/routes/app.jsx
+++ frontend/src/routes/app.jsx
@@ -1,11 +1,11 @@
-import Dashboard from 'views/Dashboard/Dashboard';
-import UserProfile from 'views/UserProfile/UserProfile';
-import TableList from 'views/TableList/TableList';
-import Typography from 'views/Typography/Typography';
-import Icons from 'views/Icons/Icons';
-import Maps from 'views/Maps/Maps';
-import Notifications from 'views/Notifications/Notifications';
-import MessengerBot from 'views/MessengerBot';
+import Dashboard from '../views/Dashboard/Dashboard';
+import UserProfile from '../views/UserProfile/UserProfile';
+import TableList from '../views/TableList/TableList';
+import Typography from '../views/Typography/Typography';
+import Icons from '../views/Icons/Icons';
+import Maps from '../views/Maps/Maps';
+import Notifications from '../views/Notifications/Notifications';
+import MessengerBot from '../views/MessengerBot';
 
 const appRoutes = [
     { path: "/dashboard", name: "Dashboard", icon: "pe-7s-graph", component: Dashboard },
